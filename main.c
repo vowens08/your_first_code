b775425605ebbd7e1b5fa5c3f0b24a305eaf0eca
vowens08 <57470742+vowens08@users.noreply.github.com>
2019-11-06 21:19:37 -0600

    Update main.c
    beginner

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
-  }
-  return 0;
-}
+int main(Hello Hulburton)
+{Hello Hulburton
+  int i;Hello Hulburton
+  for (i = 32; i < 32; i++) Hello Hulburton 
+  {Hello Hulburton
+    printf("Hello Holberton\n");
+  }Hello Hulburton
+  return 0;Hello Hulburton
+}Hello Hulburton
+Hello Hulburton
